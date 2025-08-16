1bcb2b3ff9778e652ffc02a503521ca13e1b5156
tweeks14 <124928667+tweeks14@users.noreply.github.com>
2025-08-16 21:02:34 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,17 +1,47 @@
-local DiscordLib = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/discord%20lib.txt")()
+local DiscordLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/discord%20lib.txt"))()
 
 local win = DiscordLib:Window("DevHUB")
 
 local serv = win:Server("Jailbreak", "")
+local serv1 = win:Server("Grow A Garden", "")
+local serv2 = win:Server("Steal A Brainrot", "")
+local serv3 = win:Server("Forsaken", "")
+local serv4 = win:Server("Ink Game", "")
+local serv5 = win:Server("Rivals", "")
 
-local btns = serv:Channel("Buttons")
+local btns = serv:Channel("Scripts")
+local btns1 = serv1:Channel("Scripts")
+local btns2 = serv2:Channel("Scripts")
+local btns3 = serv3:Channel("Scripts")
+local btns4 = serv4:Channel("Scripts")
+local btns5 = serv5:Channel("Scripts")
 
-btns:Button("Kill all", function()
-DiscordLib:Notification("Notification", "Killed everyone!", "Okay!")
+btns:Button("Auto Rob V6", function()
+    loadstring(game:HttpGet('http://scripts.projectauto.xyz/AutoRobV6'))()
 end)
 
 btns:Seperator()
 
-btns:Button("Get max level", function()
-DiscordLib:Notification("Notification", "Max level!", "Okay!")
+btns:Button("Universal Farm", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/BlitzIsKing/UniversalFarm/refs/heads/main/Jailbreak/autoRob"))()
+end)
+
+btns1:Button("Soluna Script", function()
+    loadstring(game:HttpGet("https://soluna-script.vercel.app/grow-a-garden.lua", true))()
+end)
+
+btns2:Button("", function()
+    
+end)
+
+btns3:Button("Load Soluna Script", function()
+    loadstring(game:HttpGet("https://soluna-script.vercel.app/grow-a-garden.lua", true))()
+end)
+
+btns4:Button("Load Soluna Script", function()
+    loadstring(game:HttpGet("https://soluna-script.vercel.app/grow-a-garden.lua", true))()
+end)
+
+btns5:Button("Load Soluna Script", function()
+    loadstring(game:HttpGet("https://soluna-script.vercel.app/grow-a-garden.lua", true))()
 end)
